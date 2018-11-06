8c513442876acc5cb5342f05c754eef6ec2b6611
HoWingYip <sploooosh884@gmail.com>
2018-11-06 22:18:03 +0800

    Add functions for buttons & scrubber

--- editor-ui/scripts.js
+++ editor-ui/scripts.js
@@ -6,6 +6,81 @@ Object.defineProperty(HTMLMediaElement.prototype, "playing", {
   }
 });
 
+/* seek bar update function */
+//credits: https://blog.teamtreehouse.com/building-custom-controls-for-html5-videos
+
+window.onload = () => {
+  //timeline video scrubber code
+  var timelineVideo = document.getElementById("timeline-video");
+  var timelineScrubber = document.getElementById("timelineScrubber");
+  var timelinePlayPauseButton = document.getElementById("timelinePlayPause");
+
+  //scrub video on scrubber drag
+  timelineScrubber.addEventListener("change", () => {
+    timelineVideo.currentTime = timelineVideo.duration * (timelineScrubber.value / 100);
+  });
+
+  //move scrubber as video plays
+  timelineVideo.addEventListener("timeupdate", () => {
+    timelineScrubber.value = 100 / timelineVideo.duration * timelineVideo.currentTime;
+  });
+
+  //pause video while scrubber is being dragged, and
+  //play it when it stops being dragged
+  timelineScrubber.addEventListener("mousedown", () => {
+    timelineVideo.pause();
+    timelinePlayPauseButton.src = "icons/pngs/Play.png";
+  });
+
+  //source video scrubber code
+  var sourceVideo = document.getElementById("source-video");
+  var sourceScrubber = document.getElementById("sourceScrubber");
+  var sourcePlayPauseButton = document.getElementById("sourcePlayPause");
+
+  //scrub video on scrubber drag
+  sourceScrubber.addEventListener("change", () => {
+    sourceVideo.currentTime = sourceVideo.duration * (sourceScrubber.value / 100);
+  });
+
+  //move scrubber as video plays
+  sourceVideo.addEventListener("timeupdate", () => {
+    sourceScrubber.value = 100 / sourceVideo.duration * sourceVideo.currentTime;
+  });
+
+  //pause video while scrubber is being dragged, and
+  //play it when it stops being dragged
+  sourceScrubber.addEventListener("mousedown", () => {
+    sourceVideo.pause();
+    sourcePlayPauseButton.src = "icons/pngs/Play.png";
+  });
+};
+
+/* skip to start functions */
+
+function timelineSkipToStart() {
+  var timelineVideo = document.getElementById("timeline-video");
+  var timelinePlayPauseButton = document.getElementById("timelinePlayPause");
+  
+  timelineVideo.pause();
+  timelineVideo.currentTime = 0;
+  timelinePlayPauseButton.src = "icons/pngs/Play.png";
+}
+
+function sourceSkipToStart() {
+  var sourceVideo = document.getElementById("source-video");
+  var sourcePlayPauseButton = document.getElementById("sourcePlayPause");
+
+  sourceVideo.pause();
+  sourceVideo.currentTime = 0;
+  sourcePlayPauseButton.src = "icons/pngs/Play.png";
+}
+
+/* rewind functions */
+
+
+
+/* play/pause functions */
+
 function timelinePlayPause() {
   //initialise video and play/pause button elements as JS vars
   var timelineVideo = document.getElementById("timeline-video");
@@ -34,4 +109,28 @@ function sourcePlayPause() {
     sourceVideo.play();
     sourcePlayPauseButton.src = "icons/pngs/Pause.png";
   }
+}
+
+/* fast-forward function */
+
+
+
+/* skip to end functions */
+
+function timelineSkipToEnd() {
+  var timelineVideo = document.getElementById("timeline-video");
+  var timelinePlayPauseButton = document.getElementById("timelinePlayPause");
+
+  timelineVideo.pause();
+  timelineVideo.currentTime = timelineVideo.duration;
+  timelinePlayPauseButton.src = "icons/pngs/Play.png";
+}
+
+function sourceSkipToEnd() {
+  var sourceVideo = document.getElementById("source-video");
+  var sourcePlayPauseButton = document.getElementById("sourcePlayPause");
+
+  sourceVideo.pause();
+  sourceVideo.currentTime = sourceVideo.duration;
+  sourcePlayPauseButton.src = "icons/pngs/Play.png";
 }
