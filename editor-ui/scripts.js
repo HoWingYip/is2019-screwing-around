7cfaac8b14efd6f17edecd4ee1769cf745812100
HoWingYip <sploooosh884@gmail.com>
2018-11-07 18:48:48 +0800

    Got timers working!

--- editor-ui/scripts.js
+++ editor-ui/scripts.js
@@ -6,10 +6,10 @@ Object.defineProperty(HTMLMediaElement.prototype, "playing", {
   }
 });
 
-/* seek bar update function */
-//credits: https://blog.teamtreehouse.com/building-custom-controls-for-html5-videos
-
 window.onload = () => {
+  /* seek bar update function */
+  //credits: https://blog.teamtreehouse.com/building-custom-controls-for-html5-videos
+
   //timeline video scrubber code
   var timelineVideo = document.getElementById("timeline-video");
   var timelineScrubber = document.getElementById("timelineScrubber");
@@ -52,6 +52,60 @@ window.onload = () => {
   sourceScrubber.addEventListener("mousedown", () => {
     sourceVideo.pause();
     sourcePlayPauseButton.src = "icons/pngs/Play.png";
+  });
+
+  /* timer update functions */
+
+  //timeline timer code
+  var timelineCurrentTimeIndicator = document.getElementById("timelineCurrentTime");
+  var timelineDurationIndicator = document.getElementById("timelineDuration");
+
+  //show timeline video duration
+  timelineVideo.addEventListener("durationchange", () => {
+    if(timelineVideo.duration < 60) {
+      timelineDurationIndicator.innerHTML = "0:" + Math.floor(timelineVideo.duration);
+    } else if (timelineVideo.duration >= 60) {
+      var durationMinutes = Math.floor(timelineVideo.duration / 60);
+      var durationSeconds = Math.floor(timelineVideo.duration - durationMinutes * 60);
+      timelineDurationIndicator.innerHTML = durationMinutes + ":" + durationSeconds;
+    }
+  });
+
+  //update timeline current time indicator
+  timelineVideo.addEventListener("timeupdate", () => {
+    if(timelineVideo.duration < 60) {
+      timelineCurrentTimeIndicator.innerHTML = "0:" + ("0" + Math.floor(timelineVideo.currentTime)).slice(-2); /* formats seconds value to 2 digits */
+    } else if(timelineVideo.duration >= 60) {
+      var currentMinutes = Math.floor(timelineVideo.currentTime / 60);
+      var currentSeconds = Math.floor(timelineVideo.currentTime - currentMinutes * 60);
+      timelineCurrentTimeIndicator.innerHTML = currentMinutes + ":" + ("0" + Math.floor(currentSeconds)).slice(-2);
+    }
+  });
+
+  //source timer code
+  var sourceCurrentTimeIndicator = document.getElementById("sourceCurrentTime");
+  var sourceDurationIndicator = document.getElementById("sourceDuration");
+
+  //show source video duration
+  sourceVideo.addEventListener("durationchange", () => {
+    if(sourceVideo.duration < 60) {
+      sourceDurationIndicator.innerHTML = "0:" + Math.floor(sourceVideo.duration);
+    } else if (sourceVideo.duration >= 60) {
+      var durationMinutes = Math.floor(sourceVideo.duration / 60);
+      var durationSeconds = Math.floor(sourceVideo.duration - durationMinutes * 60);
+      sourceDurationIndicator.innerHTML = durationMinutes + ":" + durationSeconds;
+    }
+  });
+
+  //update source current time indicator
+  sourceVideo.addEventListener("timeupdate", () => {
+    if(sourceVideo.duration < 60) {
+      sourceCurrentTimeIndicator.innerHTML = "0:" + ("0" + Math.floor(sourceVideo.currentTime)).slice(-2); /* formats seconds value to 2 digits */
+    } else if(sourceVideo.duration >= 60) {
+      var currentMinutes = Math.floor(sourceVideo.currentTime / 60);
+      var currentSeconds = Math.floor(sourceVideo.currentTime - currentMinutes * 60);
+      sourceCurrentTimeIndicator.innerHTML = currentMinutes + ":" + ("0" + Math.floor(currentSeconds)).slice(-2);
+    }
   });
 };
 
